4fc9a72f009759c58766a763a3639d08b5c6fe30
Jack <40455730+idogmat@users.noreply.github.com>
2022-08-07 22:45:09 +0300

    k

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,26 @@
 const {Router} = require('express')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config =require('../config/default.json')
+const {check,validationResult} = require('express-validator')
 const User=require('../models/User')
 const router = Router();
 
-router.post('/register',async(req,res)=>{
+router.post('/register',
+    [
+        check('email','uncorrect').isEmail(),
+        check('password','uncorrect').isLength({min:6})
+    ],
+    async (req,res)=>{
     try{
+        const errors =validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors:errors.array(),
+                message:'uncorrect data'
+            })
+        }
+
         const {email,password}=req.body
         const candidate = await User.findOne({email})
         if(candidate){
@@ -18,8 +34,39 @@ router.post('/register',async(req,res)=>{
         res.status(500).json({message:'something wrong'})
     }
 })
-router.post('/login',async(req,res)=>{
+router.post('/login',
+    [
+        check('email','uncorrect').isEmail(),
+        check('password','uncorrect').isLength({min:6})
+    ],
+    async (req,res)=>{
+        try{
+            const errors =validationResult(req)
+            if(!errors.isEmpty()){
+                return res.status(400).json({
+                    errors:errors.array(),
+                    message:'uncorrect login'
+                })
+            }
 
+            const {email,password}=req.body
+            const user = await User.findOne({email})
+            if(!user){
+                return res.status(400).json({message:'this user not find'})
+            }
+            const isMatch = await bcrypt.compare(password,user.password)
+            if(isMatch){
+                res.status(400).json({message:'this user login fail'})
+            }
+            const token = jwt.sign(
+                {userId: user.id},
+                config.get('jwtSecret'),
+            {expiresIn:'1h'}
+            )
+            res.json({token,userId:user.id})
+        } catch(e){
+            res.status(500).json({message:'something wrong'})
+        }
 })
 
 module.exports=router
