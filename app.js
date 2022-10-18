a280756cdf32707f620710819450762c0db756e9
Jack <40455730+idogmat@users.noreply.github.com>
2022-10-18 19:16:43 +0300

    g

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ app.use('/api/auth',require('./routes/auth.routes'))
 app.use('/api/link',require('./routes/link.routes'))
 app.use('/t',require('./routes/redirect.routes'))
 
-if (process.env.NODE_ENV === 'production.json') {
+if (process.env.NODE_ENV === 'production') {
     app.use('/',express.static(path.join(__dirname,'client','build')))
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
+    console.log('ok')
 }
 
 const PORT = config.get('port')||5000
