96b373fcc21ce5647e288124ef0cc013025522f8
Jack <40455730+idogmat@users.noreply.github.com>
2022-10-19 09:21:47 +0300

    g

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use('/t',require('./routes/redirect.routes'))
 if (process.env.NODE_ENV === 'production') {
     app.use('/',express.static(path.join(__dirname,'client','build')))
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'client','build'))
     })
     console.log('ok')
 }
